780958365af5c57a36890bcca91b8001534663a8
SmartestWashingMachine <ottobizness@gmail.com>
2024-07-24 14:18:10 +1000

    Change auto-redrawing font outline size.

--- src/gandy/image_redrawing/smart/utils/get_stroke_width.py
+++ src/gandy/image_redrawing/smart/utils/get_stroke_width.py
@@ -1,4 +1,5 @@
 def get_stroke_width(font_size: int):
     # return max(2, font_size // 7)
     # return max(2, font_size // 2)
-    return max(2, font_size // 3)
+    # return max(2, font_size // 3)
+    return max(2, font_size // 6)
