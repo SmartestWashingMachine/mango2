ca7de861745940889a11b73b253cf98d70e9d796
SmartestWashingMachine <ottobizness@gmail.com>
2024-07-31 12:06:45 +1000

    Fix duplicate overlapping boxes for Union detection apps.

--- src/gandy/text_detection/union_image_detection.py
+++ src/gandy/text_detection/union_image_detection.py
@@ -29,6 +29,24 @@ def box_overlaps(box, other_boxes):
 
     return None
 
+# Similar to IoU
+def box_b_in_box_a_thr(box_a, box_b):
+    x_a = max(box_a[0], box_b[0])
+    y_a = max(box_a[1], box_b[1])
+    x_b = min(box_a[2], box_b[2])
+    y_b = min(box_a[3], box_b[3])
+
+    inter = abs(max((x_b - x_a, 0)) * max((y_b - y_a), 0))
+
+    if inter == 0:
+        return 0
+
+    box_b_area = abs((box_b[2] - box_b[0]) * (box_b[3] - box_b[1]))
+
+    iou = inter / float(box_b_area)
+
+    return iou
+
 class UnionImageDetectionApp(BaseImageDetection):
     def __init__(self, td_model_app: BaseImageDetection, line_model_app: BaseImageDetection):
         """
@@ -91,6 +109,16 @@ class UnionImageDetectionApp(BaseImageDetection):
         logger.debug(f"Joined nearby line bboxes in Union variant from {n_lines_before} to {len(candidate_lines)}")
 
         bboxes = candidate_lines + candidate_tds
+
+        # Filter out boxes that overlap too much.
+        new_bboxes = []
+        for idx in range(len(bboxes)):
+            others = bboxes[:idx] + bboxes[(idx + 1):]
+
+            if not any(box_b_in_box_a_thr(box_b=bboxes[idx], box_a=o) >= 0.8 for o in others):
+                new_bboxes.append(bboxes[idx])
+        bboxes = new_bboxes
+
         bboxes = np.array(bboxes)
 
         logger.debug("Sorting union boxes...")
