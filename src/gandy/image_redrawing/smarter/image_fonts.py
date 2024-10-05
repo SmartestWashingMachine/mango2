c119f46c8600c6daf2c507cc103a87c14198fdfe
SmartestWashingMachine <ottobizness@gmail.com>
2024-10-05 14:57:35 +1000

    Allow breaking long words in Smarter.

--- src/gandy/image_redrawing/smarter/image_fonts.py
+++ src/gandy/image_redrawing/smarter/image_fonts.py
@@ -2,13 +2,32 @@ from PIL import Image
 from gandy.state.config_state import config_state
 import textwrap
 
+class CustomWrapper(textwrap.TextWrapper):
+    def _handle_long_word(self, reversed_chunks, cur_line, cur_len, width):
+        # Much like the normal text wrapper, but this one adds a hyphen to broken long words.
 
-def wrap_text(text, max_chars_per_line):
+        if width < 1:
+            space_left = 1
+        else:
+            space_left = width - cur_len
+
+        if self.break_long_words:
+            # This is the actual modification.
+            cur_line.append(reversed_chunks[-1][:space_left] + "-")
+            reversed_chunks[-1] = reversed_chunks[-1][space_left:]
+
+        elif not cur_line:
+            cur_line.append(reversed_chunks.pop())
+
+def wrap_text(text, max_chars_per_line, max_word_len_before_breaking = 22):
     """
     Given a piece of text, breaks it according to the max_chars_per_line for use in PIL.
     """
-    w = textwrap.TextWrapper(
-        width=max_chars_per_line, break_long_words=False, break_on_hyphens=False
+    max_word_len = max(len(w) for w in text.split(' '))
+    break_long_words = max_word_len >= max_word_len_before_breaking
+
+    w = CustomWrapper(
+        width=max_chars_per_line, break_long_words=break_long_words, break_on_hyphens=False
     )
     wrapped_text = w.fill(text)
 
@@ -39,5 +58,5 @@ def get_vertical_spacing(font_size: int):
     return 3 - compute_stroke_size(font_size)
 
 def print_spam(s: str):
-    #print(s)
+    print(s)
     pass
