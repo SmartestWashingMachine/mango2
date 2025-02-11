e9ea754985a130dfc880f7b9a80580c41c28c5be
SmartestWashingMachine <ottobizness@gmail.com>
2025-02-11 18:23:27 +1100

    Make ReplaceTermList more responsive to smaller window sizes.

--- js-src/src/views/GlobalOptionsView/components/ReplaceTermsList.tsx
+++ js-src/src/views/GlobalOptionsView/components/ReplaceTermsList.tsx
@@ -9,6 +9,8 @@ import {
   Stack,
   TextField,
   Typography,
+  useMediaQuery,
+  useTheme,
 } from "@mui/material";
 import React from "react";
 import IReplaceTerm from "../../../types/ReplaceTerm";
@@ -26,6 +28,104 @@ export type ReplaceTermsListProps = {
   removeTerm: (termUuid: string) => void;
   importTerms: () => void;
   exportTerms: () => void;
+};
+
+type TermProps = {
+  t: IReplaceTerm;
+} & Pick<ReplaceTermsListProps, "updateTerm" | "removeTerm">;
+
+const Term = (props: TermProps) => {
+  const { updateTerm, removeTerm, t } = props;
+
+  const theme = useTheme();
+  const matchDownMd = useMediaQuery(theme.breakpoints.down("sm"));
+
+  const content = (
+    <>
+      <TextField
+        placeholder="Pattern"
+        variant="outlined"
+        onChange={(e) => updateTerm(t.uuid, "original", e.currentTarget.value)}
+        defaultValue={t.original}
+        fullWidth
+        multiline
+        size="small"
+      />
+      <TextField
+        placeholder="Replacement"
+        variant="outlined"
+        onChange={(e) =>
+          updateTerm(t.uuid, "replacement", e.currentTarget.value)
+        }
+        defaultValue={t.replacement}
+        fullWidth
+        multiline
+        size="small"
+      />
+      <TextField
+        onChange={(e) => updateTerm(t.uuid, "onSide", e.target.value)}
+        defaultValue={t.onSide}
+        variant="outlined"
+        select
+        fullWidth
+        size="small"
+      >
+        <MenuItem value="source">Source Side</MenuItem>
+        <MenuItem value="target">Target Side</MenuItem>
+      </TextField>
+      <FormControlLabel
+        control={
+          <Checkbox
+            onChange={(e) =>
+              updateTerm(t.uuid, "enabled", e.currentTarget.checked)
+            }
+            checked={t.enabled || false}
+            color="info"
+          />
+        }
+        label={matchDownMd ? "Enabled" : ""}
+        color="info"
+      />
+      {matchDownMd ? (
+        <Button
+          variant="outlined"
+          onClick={() => removeTerm(t.uuid)}
+          color="error"
+        >
+          Remove
+        </Button>
+      ) : (
+        <IconButton onClick={() => removeTerm(t.uuid)}>
+          <RemoveIcon />
+        </IconButton>
+      )}
+    </>
+  );
+
+  if (matchDownMd) {
+    return (
+      <Stack
+        spacing={1}
+        direction="column"
+        key={t.uuid}
+        justifyContent="space-around"
+        sx={{ opacity: t.enabled ? 1 : 0.5, marginTop: 2, marginBottom: 8 }}
+      >
+        {content}
+      </Stack>
+    );
+  } else
+    return (
+      <Stack
+        spacing={2}
+        direction="row"
+        key={t.uuid}
+        justifyContent="space-around"
+        sx={{ opacity: t.enabled ? 1 : 0.5, marginTop: 2 }}
+      >
+        {content}
+      </Stack>
+    );
 };
 
 const ReplaceTermsList = ({
@@ -54,69 +154,18 @@ const ReplaceTermsList = ({
         </Grid>
       </Grid>
       {terms.map((t) => (
-        <Stack
-          spacing={2}
-          direction="row"
-          key={t.uuid}
-          justifyContent="space-around"
-          sx={{ opacity: t.enabled ? 1 : 0.5, marginTop: 2 }}
-        >
-          <TextField
-            placeholder="Pattern"
-            variant="outlined"
-            onChange={(e) =>
-              updateTerm(t.uuid, "original", e.currentTarget.value)
-            }
-            defaultValue={t.original}
-            fullWidth
-            multiline
-            size="small"
-          />
-          <TextField
-            placeholder="Replacement"
-            variant="outlined"
-            onChange={(e) =>
-              updateTerm(t.uuid, "replacement", e.currentTarget.value)
-            }
-            defaultValue={t.replacement}
-            fullWidth
-            multiline
-            size="small"
-          />
-          <TextField
-            onChange={(e) => updateTerm(t.uuid, "onSide", e.target.value)}
-            defaultValue={t.onSide}
-            variant="outlined"
-            select
-            fullWidth
-            size="small"
-          >
-            <MenuItem value="source">Source Side</MenuItem>
-            <MenuItem value="target">Target Side</MenuItem>
-          </TextField>
-          <FormControlLabel
-            control={
-              <Checkbox
-                onChange={(e) =>
-                  updateTerm(t.uuid, "enabled", e.currentTarget.checked)
-                }
-                checked={t.enabled || false}
-                color="info"
-              />
-            }
-            label=""
-            color="info"
-          />
-          <IconButton onClick={() => removeTerm(t.uuid)}>
-            <RemoveIcon />
-          </IconButton>
-        </Stack>
+        <Term t={t} updateTerm={updateTerm} removeTerm={removeTerm} />
       ))}
       <Button variant="outlined" color="primary" onClick={createTerm}>
         Create Term
       </Button>
       <Stack direction="row" spacing={2}>
-        <Button variant="outlined" color="secondary" onClick={importTerms} fullWidth>
+        <Button
+          variant="outlined"
+          color="secondary"
+          onClick={importTerms}
+          fullWidth
+        >
           Import Terms
         </Button>
         <Button variant="outlined" color="info" onClick={exportTerms} fullWidth>
