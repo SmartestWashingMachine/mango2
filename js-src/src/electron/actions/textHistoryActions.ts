5aa143b90d0982457902cbdf0739b63d1fdd551b
SmartestWashingMachine <ottobizness@gmail.com>
2025-02-11 16:33:47 +1100

    Allow generic translation streaming to work for last N items.

--- js-src/src/electron/actions/textHistoryActions.ts
+++ js-src/src/electron/actions/textHistoryActions.ts
@@ -2,14 +2,30 @@ import ElectronCommands from "../../types/ElectronCommands";
 import { v4 as uuidv4 } from "uuid";
 import { GatewayAction } from "../../types/GatewayAction";
 
-const prettifyTokens = (tokens: string[]) =>
-  tokens.map((t) =>
-    t
-      .replace("▁", " ")
-      .replace("<pad>", "<start> ")
-      .replace("</s>", " <end>")
-      .replace("<SEP", " <SEP")
-  );
+const modifyLastNItems = <T>(
+  array: T[],
+  n: number,
+  predicate: (item: T) => boolean,
+  modifier: (item: T) => T,
+  breakIfModified = true
+) => {
+  let arrayWasModified = false;
+
+  const startIndex = Math.max(array.length - n, 0);
+
+  // Notice this iterates in reverse-order. Since this is used for updating text history via streamed updates, it's more likely that the item to update is at the bottom or end of the list.
+  for (let i = array.length - 1; i >= startIndex; i--) {
+    if (predicate(array[i])) {
+      arrayWasModified = true;
+
+      array[i] = modifier(array[i]);
+
+      if (breakIfModified) break;
+    }
+  }
+
+  return arrayWasModified;
+};
 
 const retrieveTextHistory: GatewayAction = {
   command: ElectronCommands.RETRIEVE_TEXT_HISTORY,
@@ -37,14 +53,18 @@ const addToTextHistory: GatewayAction = {
       };
 
       // Replace instead if the last ID is the same. Used for generic translation tasks (/translate).
-      if (
-        state.texts.length > 0 &&
-        state.texts[state.texts.length - 1].uuid === text.uuid &&
-        state.texts[state.texts.length - 1].targetText[0].length < // Nested lists. I forgot why.
-          text.targetText[0].length
-      ) {
-        state.texts[state.texts.length - 1] = text;
-      } else {
+      const wasModified = modifyLastNItems(
+        state.texts,
+        10, // Iterate over last 10 items.
+        (
+          stateText // Modify that item (stateText) if these conditions are met (same ID & less length).
+        ) =>
+          stateText.uuid === text.uuid &&
+          stateText.targetText[0].length < text.targetText[0].length, // Nested lists. I forgot why.
+        () => text // If the predicate (previous parameter) is true, replace that item with our new text.
+      );
+
+      if (!wasModified) {
         state.texts.push(text);
       }
     }
