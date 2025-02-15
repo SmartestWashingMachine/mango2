e11e47bcf3a7083500662ad75881a7c2dc4f1511
SmartestWashingMachine <ottobizness@gmail.com>
2025-02-16 08:15:44 +1100

    Undo size-dependent blur clean - too difficult to get right with current implementation.

--- src/gandy/image_cleaning/blur_image_clean.py
+++ src/gandy/image_cleaning/blur_image_clean.py
@@ -11,15 +11,13 @@ class BlurImageCleanApp(BaseImageClean):
 
     def process(self, image, bboxes):
         input_image = image.copy()
+        blurred_image = input_image.filter(ImageFilter.GaussianBlur(5))
 
         # Size gives width, height.
         mask = np.zeros(
             (input_image.size[1], input_image.size[0]),
             dtype=np.uint8,
         )  # 255 for text regions. 0 otherwise.
-        mask = Image.fromarray(mask, mode="L")
-
-        img_area = max(1, input_image.width * input_image.height)
 
         for s in bboxes:
             x1, y1, x2, y2 = s
@@ -28,28 +26,10 @@ class BlurImageCleanApp(BaseImageClean):
             x2 = floor(x2)
             y2 = floor(y2)
 
-            region = input_image.crop((x1, y1, x2, y2)) # Replaces other blurs since we're not using input_image.
+            mask[y1:y2, x1:x2] = 255
 
-            box_area = (y2 - y1) * (x2 - x1)
-            blur_radius = int((box_area / img_area) * 100 * 10)
+        mask = Image.fromarray(mask, mode="L")
+        mask = mask.filter(ImageFilter.GaussianBlur(6))
 
-            blurred_region = region.filter(ImageFilter.GaussianBlur(blur_radius))
-
-            input_image.paste(blurred_region, (x1, y1))
-
-            # Now we want to smoothen the edges of the blurred regions.
-            # 1. Create a separate mask for each bounding box.
-            temp_mask = np.zeros(
-                (input_image.size[1], input_image.size[0]),
-                dtype=np.uint8,
-            )
-            temp_mask[y1:y2, x1:x2] = 255
-            temp_mask = Image.fromarray(temp_mask, mode="L")
-            temp_mask = temp_mask.filter(ImageFilter.GaussianBlur(10))
-
-            # 2. Combine the temporary mask with the main mask.
-            mask = Image.composite(temp_mask, mask, temp_mask)
-
-        mask = mask.filter(ImageFilter.GaussianBlur(10))  # Additional blur for smoother edges
-
-        return Image.composite(input_image, image, mask)
+        input_image.paste(blurred_image, mask)
+        return input_image
