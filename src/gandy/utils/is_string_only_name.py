8f30bc5a2464aea59139d5916ee96e71e7f763a1
SmartestWashingMachine <ottobizness@gmail.com>
2025-08-05 13:17:47 +1000

    Remove log as it causes error in name detection...

--- src/gandy/utils/is_string_only_name.py
+++ src/gandy/utils/is_string_only_name.py
@@ -24,8 +24,24 @@ class NameChecker():
                 pos_main = word.feature[0]
                 pos_sub1 = word.feature[1] if len(word.feature) > 1 else ''
 
-                ctx.log(f"Token: {word.surface:<10} POS1: {pos_main:<10} POS2: {pos_sub1:<10}")
-                print(f"Token: {word.surface:<10} POS1: {pos_main:<10} POS2: {pos_sub1:<10}")
+                try:
+                    # Fudging Eliot can't log special unicode characters because OF COURSE IT CAN'T. WHY DOES A LOGGER HAVE LOGGING BUGS.
+                    # IT'S LIKE THEY DON'T WANT ME TO LOG THINGS.
+                    # IT'S LIKE A VILLAGE THAT LIVES IN AN AREA PRONE TO FIRES - THEY BUILT THEIR HOUSES OUT OF WOOD WITHOUT THINKING...
+                    # SO THE VILLAGE ELDER IS LIKE: "OH. IT'S OK - WE'LL BUILD AND STAFF A FIRE STATION!"
+                    # "AND WHENEVER OUR LOOKOUTS SPOT A FIRE, EVERYONE IN THE VILLAGE WILL STOP WHATEVER THEY ARE DOING AND ESCAPE TO THE TOWN SQUARE WHILE THE FIRE IS BEING PUT OUT!"
+                    # UNTIL YOU FIND OUT THE FUDGING FIRE STATION IS ALSO MADE OUT OF WOOD AND THE FIRE STATION IS THE ONLY ONE THAT KEEPS GETTING BURNED DOWN.
+
+                    # DID YOU KNOW YOU CAN'T EVEN *READ* THE ELIOT LOGS WITH THE TREE UTILITY BY DEFAULT? I HAD TO WRITE MY OWN UTILITY TO PARSE UNICODE.
+                    # AND WHY DOES ELIOT CATCH EXCEPTIONS WITHIN EVENTS WITHOUT RE-PROPAGATING IT? IN WHAT WORLD DOES A LOGGER HAVE THE RIGHT TO AFFECT THE SYSTEM???
+                    # I CAN'T EVEN PROPERLY STITCH THE TRACES ACROSS DIFFERENT MACHINES - THE TIMESTAMPS ARE ALL MESSED UP?????
+                    # WHY DO THEY EVEN NEED TEN THOUSAND DIFFERENT CODE FILES IN THEIR LIBRARY? WHY???
+                    # THE LESSON HERE IS: FIND A DIFFERENT LOGGER.
+
+                    # ctx.log(f"Token: {word.surface:<10} POS1: {pos_main:<10} POS2: {pos_sub1:<10}")
+                    print(f"Token: {word.surface:<10} POS1: {pos_main:<10} POS2: {pos_sub1:<10}")
+                except:
+                    pass
 
                 # If there's a verb, it's probably not a name.
                 if pos_main == '動詞' or pos_main == '助動詞':
