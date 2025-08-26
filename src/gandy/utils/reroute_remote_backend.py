efae84f07c38dcb562343db844617cd732b1d509
SmartestWashingMachine <ottobizness@gmail.com>
2025-08-26 13:08:59 +1000

    Move remote router sending to another thread - should ensure remote task3 calls return much faster, making the boxes visible again quicker.

--- src/gandy/utils/reroute_remote_backend.py
+++ src/gandy/utils/reroute_remote_backend.py
@@ -4,7 +4,53 @@ import io
 import base64
 from PIL import Image
 import requests
+from dataclasses import dataclass
+import threading
+import queue
 from time import sleep
+from typing import Any
+
+@dataclass
+class ProcessItem():
+    addr: str
+    data: Any
+    files: Any
+
+# Thread-safe.
+message_queue = queue.Queue()
+
+class RemoteRouterProcess(threading.Thread):
+    def __init__(self):
+        super().__init__()
+        self.session = requests.Session()
+
+    def make_call(self, item: ProcessItem):
+        while True:
+            try:
+                response = self.session.post(item.addr, data=item.data, files=item.files)
+                response.raise_for_status()
+
+                print(f"Request to {item.addr} successful.")
+                return
+            except (requests.exceptions.ConnectionError, requests.exceptions.Timeout) as e:
+                self.session = requests.Session()
+                print('Session error. Retrying session:')
+                print(e)
+
+                sleep(1) 
+            except Exception as e:
+                print('Error FORM POSTING to remote router:')
+                print(e)
+                raise
+
+    def run(self):
+        while True:
+            item = message_queue.get(block=True)
+            self.make_call(item)
+
+    # Not even sure if this is necessary, or can be called. Either way all processes / threads should be stopped when the app closes.
+    def stop(self):
+        del self.session
 
 class RemoteRouter():
     def __init__(self, socketio_address: str, compress_jpeg: bool):
@@ -13,6 +59,9 @@ class RemoteRouter():
         self.compress_jpeg = compress_jpeg
 
         self.session = requests.Session()
+
+        self.remote_router_process = RemoteRouterProcess()
+        self.remote_router_process.start()
 
     def base64_to_pil(self, base64_images):
         images = []
@@ -36,36 +85,11 @@ class RemoteRouter():
             images.append(base64.b64encode(img_byte).decode('utf-8'))
 
         return images
-    
-    def form_post(self, addr: str, data, files = None):
-        old_addr = addr
-        addr = 'http://' + self.socketio_address + ':5000' + addr
 
-        try:
-            response = self.session.post(addr, data=data, files=files)
-            response.raise_for_status()
-        except (requests.exceptions.ConnectionError, requests.exceptions.Timeout) as e:
-            self.session = requests.Session()
-            print('Session error. Retrying session:')
-            print(e)
-            sleep(1)
+    def form_post(self, addr: str, data, files=None):
+        addr = f'http://{self.socketio_address}:5000{addr}'
 
-            self.form_post(old_addr, data, files)
-        except Exception as e:
-            print('Error FORM POSTING to remote router:')
-            print(e)
-            raise e
-
-    def post(self, addr: str, data):
-        addr = 'http://' + self.socketio_address + ':5000' + addr
-
-        response = requests.post(addr, json=data)
-        try:
-            response.raise_for_status()
-        except Exception as e:
-            print('Error JSON POSTING to remote router:')
-            print(e)
-            raise e
+        message_queue.put(ProcessItem(addr=addr, data=data, files=files))
 
     def is_remote(self):
         return self.socketio_address is not None and self.socketio_address.strip() != '' and self.socketio_address != '127.0.0.1'
