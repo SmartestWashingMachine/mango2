e0205ff389722c1685dce2352e38747265e1c97f
SmartestWashingMachine <ottobizness@gmail.com>
2025-12-07 18:28:16 +1100

    Remove legacy models for text line user-facing options.

--- js-src/src/utils/appOptions/textLineOptions.ts
+++ js-src/src/utils/appOptions/textLineOptions.ts
@@ -20,33 +20,4 @@ export const TEXT_LINE_OPTIONS = [
     Slightly weaker but slightly faster than D-FINE Line EXO-Massive.
     `,
   },
-  {
-    name: "(Legacy) DETR Line EX",
-    value: "yolo_line_e",
-    desc: `
-    Can scan lines individually and works well with the "None" text detection app for certain games. Nearby lines will be translated as one unit.
-    `,
-  },
-  {
-    name: "(Legacy) YOLO Line Light",
-    value: "yolo_line_light",
-    desc: `
-    Fast and filters out noise. Must be used with a Text Detection model.
-    `,
-  },
-  {
-    name: "(Legacy) YOLO Line",
-    value: "yolo_line",
-    desc: `
-    Attempts to detect each text line AFTER detecting speech
-    bubbles. Slower.
-    `,
-  },
-  {
-    name: "(Legacy) YOLO Line XL",
-    value: "yolo_line_xl",
-    desc: `
-    Stronger but slower than YOLO Line.
-    `,
-  },
 ];
