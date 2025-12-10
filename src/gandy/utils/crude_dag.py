503d51cc31b95671c4aa52ab520bee9459d385be
SmartestWashingMachine <ottobizness@gmail.com>
2025-12-10 15:22:49 +1100

    Add crude safety check to ensure no cycles when sorting frames. Ideally this would never happen though...

--- src/gandy/utils/crude_dag.py
+++ src/gandy/utils/crude_dag.py
@@ -11,11 +11,36 @@ class CrudeDAG():
             self.adj_list[node] = []
             self.in_degree[node] = 0
 
+    def _reachable(self, start, target):
+        # Return True if `target` is reachable from `start` (DFS).
+        if start == target:
+            return True
+        visited = set()
+        stack = [start]
+        while stack:
+            node = stack.pop()
+            if node == target:
+                return True
+            if node in visited:
+                continue
+            visited.add(node)
+            for neighbor in self.adj_list.get(node, []):
+                if neighbor not in visited:
+                    stack.append(neighbor)
+        return False
+
     def add_edge(self, from_node, to_node):
         self.add_node(from_node)
         self.add_node(to_node)
+
+        # If there is already a path from `to_node` back to `from_node`,
+        # adding `from_node -> to_node` would create a cycle. Do a no-op.
+        if self._reachable(to_node, from_node):
+            return False
+
         self.adj_list[from_node].append(to_node)
         self.in_degree[to_node] += 1
+        return True
 
     def topological_sort(self):
         zero_in_degree = [node for node in self.in_degree if self.in_degree[node] == 0]
@@ -23,6 +48,7 @@ class CrudeDAG():
 
         while zero_in_degree:
             node = zero_in_degree.pop()
+
             sorted_nodes.append(node)
 
             for neighbor in self.adj_list[node]:
