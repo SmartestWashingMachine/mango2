6fb62f13645d5185baec8b45dd9cc5b22d372b22
SmartestWashingMachine <ottobizness@gmail.com>
2026-02-27 12:19:59 +1100

    Remove unnecessary llama-cpp-python build hooks.

--- src/build_hooks/hook-llama_cpp.py
+++
@@ -1,5 +0,0 @@
-from PyInstaller.utils.hooks import collect_dynamic_libs
-
-# Automatically collect all shared libraries
-binaries = collect_dynamic_libs('llama_cpp')
-print(f"🚀 Hook executed at compile time: {binaries}")
--- src/build_hooks/hook-llama_cpp_cuda.py
+++
@@ -1,5 +0,0 @@
-from PyInstaller.utils.hooks import collect_dynamic_libs
-
-# Automatically collect all shared libraries
-binaries = collect_dynamic_libs('llama_cpp_cuda')
-print(f"🚀 Hook executed at compile time CUDA: {binaries}")
